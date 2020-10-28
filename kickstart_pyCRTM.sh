df8216a5f53c0b259a54a49f4e417fe633aca401
BenjaminTJohnson <Benjamin.T.Johnson@noaa.gov>
2020-10-28 15:07:45 +0000

    fix: updated kickstart_pyCRTM.sh

--- kickstart_pyCRTM.sh
+++ kickstart_pyCRTM.sh
@@ -1,8 +1,4 @@
 #simple script to kickstart pycrtm
-cd crtm
-echo "[kickstart] getting remote binary data."
-sh Get_CRTM_Binary_Files.sh
-cd ..
 mkdir crtm_v2.4.0
 cd crtm_v2.4.0
 echo "[kickstart] making crtm in crtm-bundle/crtm_v2.4.0:"
@@ -12,9 +8,16 @@ make -j8
 echo "[kickstart] linking to modules:"
 ln -s `find ./ -name "crtm_module.mod" | sed 's/crtm_module.mod//'` ./include
 cd ..
+
+cd crtm
+echo "[kickstart] getting remote binary data."
+sh Get_CRTM_Binary_Files.sh
+cd ..
+
 echo "[kickstart] installing pycrtm"
 cd pycrtm
 ./setup_pycrtm.py  --install $PWD/../ --repos $PWD/../crtm/ --jproc 1 --coef ./ --ncpath /usr/local/ --h5path /usr/local/ --arch gfortran --inplace
+ln -s $PWD/../crtm/crtm_coef_pycrtm $PWD
 echo "[kickstart] running a testcase."
 ./testCases/test_cris.py
 echo "[kickstart] Read the README.md file for more information (note *_threads.py files currently not functional)."
